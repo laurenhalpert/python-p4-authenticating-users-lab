925c3b378ff8f822f84280a59ee79fbbe4fac2f2
Lauren <halpertln@gmail.com>
2023-07-21 10:41:51 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,30 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict()
+api.add_resource(Login, '/login')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {} , 401
+api.add_resource(CheckSession, '/check_session')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+api.add_resource(Logout, '/logout')
+
+
 class ClearSession(Resource):
 
     def delete(self):
